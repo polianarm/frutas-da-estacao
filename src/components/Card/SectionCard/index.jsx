843fda7daf293dc4ac73147c923b2e57fcef53f0
Poliana <polianariveiro@gmail.com>
2024-01-26 00:50:40 -0300

    comecando os inputs

--- src/components/Card/SectionCard/index.jsx
+++ src/components/Card/SectionCard/index.jsx
@@ -5,7 +5,37 @@ import frutas from "../../../data/frutas.json";
 export default function SectionCard() {
   return (
     <section id="frutas" className={styles.section}>
-      <h2 className={styles.titleSection}>BUSCAR FRUTAS</h2>
+      {/* buscar por nome */}
+
+      <div className={styles.filtroNome}>
+        <h2 className={styles.titleSection}>BUSCAR FRUTAS</h2>
+        <input
+          type="text"
+          value=""
+          onChange=""
+          placeholder="Digite o nome da fruta"
+        />
+      </div>
+
+      {/* busca por sabor */}
+
+      <div className={styles.filtroSabor}>
+        <p>Sabor</p>
+        <select value="All" onChange="">
+          <option value="All">Todos</option>
+          <option value="Doce">Doce</option>
+          <option value="Ácido">Ácido</option>
+          <option value="Agridoce">Agridoce</option>
+        </select>
+      </div>
+
+      {/* buscar por estação */}
+
+      <div></div>
+
+      {/* ordenar por nome crescente ou decrescente */}
+
+      <div></div>
 
       <div className={styles.sectionCard}>
         {frutas.map((item, index) => (
